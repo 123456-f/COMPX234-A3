c8f96e4cf73613565dfb3216f1a9224b2067afda
123456-f <385655160@qq.com>
2026-04-28 09:13:04 +0800

    task4 change1

--- TupleSpaceClient_help-1.py
+++ TupleSpaceClient_help-1.py
@@ -101,6 +101,7 @@ def main():
         sys.exit(1)
     finally:
         # TASK 4: Close the socket when done (already called for you — explain why
+        # reason:Ensure that sock. close() will definitely execute
         # finally: is the right place to do this even if an error occurs above).
         sock.close()
 
